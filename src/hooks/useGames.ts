5b7f3b77c1a1bf696e3c818260b72dcd93ef1309
deekeej <96180674+deekeej@users.noreply.github.com>
2023-04-10 19:20:18 +0200

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,16 +3,17 @@ import {useEffect, useState} from 'react';
 import apiClient from '../services/api-client';
 
 export interface Platform{
-    id: number,
-    name:string,
-    slug:string
+    id: number;
+    name:string;
+    slug:string;
 }
 
 export interface Game {
     id: number;
     name: string;
-    background_image:string
-    parent_platforms: {platform:Platform}[]
+    background_image:string;
+    parent_platforms: {platform:Platform}[];
+    metacritic: number;
   }
   
   interface FetchGamesResponse {
